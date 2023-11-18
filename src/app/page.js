aee1b2cfd66285bba2ba16f86a90083dfbd511f6
Shawn <Shawn@DESKTOP-U22QHSI>
2023-11-18 23:44:09 +0800

    add dark mode

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import RootLayout from './layout'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-400'>Home</h1>
+    <div> </div>
   )
 }
