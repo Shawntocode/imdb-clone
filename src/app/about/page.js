b40def79fb750669e1e84be0552a9f81ad0d34e0
Shawn <Shawn@DESKTOP-U22QHSI>
2023-11-19 01:24:05 +0800

    create the about page

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 space-y-4 ">About</h1>
+      <p>
+        This is a clone movie database website!
+      </p>
+
+      <p>
+        這是一個仿照IMDb的網站!
+      </p>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import RootLayout from './layout'
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <div> </div>
